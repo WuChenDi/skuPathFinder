957360131237f4682f536f594677446e916886de
WuChenDi <wuchendi96@gmail.com>
2022-08-24 10:47:36 +0000

    Deploying to gh-pages from @ WuChenDi/skuPathFinder@bbb3a9d851b203dc11016b6cc5a606ac19cf4045 🚀

---
+++ _next/static/Jd9OLmof5Na3n-gM_VPkY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/745-6108bf553f27c894.js","static/chunks/pages/index-a0337923154de072.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
